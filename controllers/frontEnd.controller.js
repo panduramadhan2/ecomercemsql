ced8026a5b1ca387da8d5430cb8b8ed67f53595c
panduramadhan2 <panduramadhan2@gmail.com>
2023-06-28 16:32:28 +0700

    checkpoint frontend produk detil

--- controllers/frontEnd.controller.js
+++ controllers/frontEnd.controller.js
@@ -2,10 +2,12 @@ const db = require("../models/bundle.model");
 const Op = db.Sequelize.Op;
 const func = require("../libs/function");
 const { v4: uuidv4 } = require("uuid");
+const { where } = require("sequelize");
 
 exports.getProdukHome = async (req, res) => {
   db.produk
     .findAll({
+      attributes: ["id", "title", "image", "price", "url"],
       limit: 8,
     })
     .then((result) => {
@@ -22,10 +24,91 @@ exports.getProdukHome = async (req, res) => {
         });
       }
     })
-    .cacth((err) => {
+    .catch((err) => {
       res.status(500).send({
         code: 500,
         message: "Error find Data >" + err,
+      });
+    });
+};
+
+exports.getProdukPage = async (req, res) => {
+  let keyword = "";
+  const condition = [];
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    condition.push({
+      title: { [Op.like]: "%" + keyword + "%" },
+    });
+  }
+
+  db.produk
+    .findAll({
+      where: condition,
+      attributes: ["id", "title", "image", "price", "url"],
+    })
+    .then((result) => {
+      if (result.length > 0) {
+        res.send({
+          code: 200,
+          message: "OK",
+          data: result,
+        });
+      } else {
+        res.status(404).send({
+          code: 404,
+          message: `Tidak ada data yang cocok pada keyword '${keyword}'`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        code: 500,
+        message: "Error find Data >" + err,
+      });
+    });
+};
+
+exports.getProdukDetil = async (req, res) => {
+  const url = req.params.url;
+  db.produk
+    .findOne({
+      where: { url: url },
+      attributes: [
+        "id",
+        "title",
+        "description",
+        "full_description",
+        "price",
+        "image",
+        "url",
+        "createdAt",
+      ],
+      include: [
+        {
+          model: db.kategori,
+          attributes: ["name"],
+        },
+      ],
+    })
+    .then((result) => {
+      if (result) {
+        res.send({
+          code: 200,
+          message: "OK",
+          data: result,
+        });
+      } else {
+        res.status(404).send({
+          code: 404,
+          message: "Produk telah dihapus!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        code: 500,
+        message: "Error retrive data",
       });
     });
 };
--- routes/frontEnd.js
+++ routes/frontEnd.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const frontEnd = require("../controllers/frontEnd.controller");
 
-
+router.get("/produkHome", frontEnd.getProdukHome);
+router.get("/produkPage", frontEnd.getProdukPage);
+router.get("/produkDetil/:url", frontEnd.getProdukDetil);
 
 module.exports = router;
